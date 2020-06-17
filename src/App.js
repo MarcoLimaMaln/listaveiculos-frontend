e84b646769a9f756b7bee2c8db75535a9655e021
MarcoLimaMaln <marco.lima4@fatec.sp.gov.br>
2020-06-17 23:11:04 +0000

    17062020

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom'
+import api from './api';
 
 function App() {
-  return <h1>ReactJS, Template!</h1>
+    
+    const [ lista, setLista ] = useState([]);
+    const [ loading, setLoading ] = useState(true);
+
+    useEffect(() => {
+        api.get('/carro').then((response) => {
+            const itens = response.data;
+            setLista(itens);
+            setLoading(false);
+        })
+    }, [])
+
+    return(
+        <>
+            { loading ? <span>Carregando Dados...</span> : <div/> }
+            <table>
+                {lista.map(item => (
+                    <tr key={item.id}>
+                        <td>{item.id}</td>
+                        <td>{item.modelo}</td>
+                        <td>{item.fabricante}</td>
+                        <td>{item.ano}</td>
+                    </tr>
+                ))}
+            </table>
+            <Link to="create">Adicionar</Link> 
+        </>
+  
+  );
+
 }
 
 export default App;
